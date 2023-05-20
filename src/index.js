bb01726a4192e9bb5bf963f3e465a08974dc1b39
karanukvova <volodimirkaranuk@gmail.com>
2023-05-20 14:22:20 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 const refs = {
     input : document.getElementById("search-box"),
-    countryList: document.getElementsByClassName("country-list"),
-    countryInfo : document.getElementsByClassName("country-info")
+    countryList: document.querySelector(".country-list"),
+    countryInfo : document.querySelector(".country-info")
 }
 refs.input.addEventListener("input", debounce(inputName, DEBOUNCE_DELAY))
 function inputName() {
+    refs.countryInfo.innerHTML = ''
+    refs.countryList.innerHTML = ''
     const value = refs.input.value.trim()
     if (value === "") return
     fetchCountries(value)
@@ -19,19 +21,30 @@ function checkResult(check) {
     if(check.length > 10) {
         return Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
     } else if (check.length === 1) {
-        const string = renderContryInfo(check)
+        const string = renderCountryInfo(check)
         refs.countryInfo.insertAdjacentHTML("beforeend", string)
+    } else {
+        const list = renderCountryList(check)
+        refs.countryList.insertAdjacentHTML("beforeend", list)
     }
 }
-function renderContryInfo(countries) {
+function renderCountryInfo(countries) {
     return countries.reduce((acc, { name: { official }, capital, population, flags, languages }) => {
         languages = Object.values(languages).join(', ');
         return (acc + `
             <img src="${flags.svg}" width="50"/>
             <h1>${official}</h1>
             <h2>Capital: ${capital}</h2>
-            <p>Population: ${population}</p>
+            <p>Population: ${population.toLocaleString()}</p>
             <p>Languages: ${languages}</p>
+        `);
+    }, '')
+}
+function renderCountryList(countries) {
+    return countries.reduce((acc, { name: { common }, flags}) => {
+        return (acc + `
+            <img src="${flags.svg}" width="30"/>
+            <p>${common}</p>
         `);
     }, '')
 }
